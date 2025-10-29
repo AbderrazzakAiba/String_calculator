f54d8f15472187a438ed3b4d7055ccfcbc130e55
Abderrazzak Aiba <aibaabderrazzak7@gmail.com>
2025-10-29 21:58:26 +0100

    refactor

--- test/test_String_calculator.py
+++ test/test_String_calculator.py
@@ -3,11 +3,19 @@ import pytest
 from src.String_calculator import Add
 @pytest.mark.parametrize("string, expected_result", [
     ("", 0),
+    ("0", 0),
     ("1", 1),
     ("2", 2),
-    ("3", 3)
+    ("3", 3),
+    ("9", 9),
+    ("1,2", 3),
+    ("2,3", 5),
+    ("5,5", 10),
+    ("10,20", 30),
+    ("7,8", 15),
+    ("100,200", 300),
+    ("99,1", 100),
 ])
 def test_number_return_itself(string, expected_result):
     assert Add(string) == expected_result
-def test_two_numbers_return_sum():
-    assert Add("1,2") == 3
+
